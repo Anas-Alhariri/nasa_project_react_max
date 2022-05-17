8f253b50bc53a75c8992c1631a67d389550fe88b
Anas Alhariri (Max) <eng.anas.alhariri@gmail.com>
2022-05-17 11:46:29 -0400

    Now Favorites page shows only the cards that have copyright text in it.

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import CardsList from "../components/cardsList/CardsList";
 
 const Favorites = ({ cards }) => {
-  return <CardsList cards={cards} />;
+  const favList = cards.filter((card) => card.copyright);
+  return <CardsList cards={favList} />;
 };
 
 export default Favorites;
