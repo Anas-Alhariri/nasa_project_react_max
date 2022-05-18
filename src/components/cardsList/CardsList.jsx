5f861779700d0c0ece5d36e5d8644c7850486e6c
Anas Alhariri (Max) <eng.anas.alhariri@gmail.com>
2022-05-18 13:44:48 -0400

    Modified the CardsList to allow to show or hide loader when it is called.

--- src/components/cardsList/CardsList.jsx
+++ src/components/cardsList/CardsList.jsx
@@ -2,9 +2,9 @@ import React, { useEffect } from "react";
 import Card from "../card/Card";
 import Loader from "../loader/Loader";
 
-const CardsList = ({ cards }) => {
+const CardsList = ({ cards, hideLoader }) => {
   const loadCards = () => {
-    if (cards.length <= 0) {
+    if (cards.length <= 0 && !hideLoader) {
       return <Loader />;
     }
 
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import CardsList from "../components/cardsList/CardsList";
 import { getFavItemsFromLocalStorage } from "../components/localstorage/LocalStorageManager";
 
-const Favorites = () => {
+const Favorites = ({ hideLoader }) => {
   const [faveItemsList, setFaveItemsList] = React.useState([]);
   React.useEffect(() => {
     setFaveItemsList(getFavItemsFromLocalStorage());
   }, []);
 
-  return <CardsList cards={faveItemsList} />;
+  return <CardsList cards={faveItemsList} hideLoader={true} />;
 };
 
 export default Favorites;
